f6df41a1944dda7be2399edf4019893673c1d3cf
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-09-16 22:56:17 +0200

    WIP

--- nvim/lua/lucasd/plugins/lsp/mason.lua
+++ nvim/lua/lucasd/plugins/lsp/mason.lua
@@ -1,30 +1,37 @@
-local mason_status, mason = pcall(require,"mason")
+local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
-local mason_lspconfig_status, mason_lspconfig = pcall(require,"mason-lspconfig")
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
-local mason_null_ls_status, mason_null_ls = pcall(require,"mason-null-ls")
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
-
 
 mason.setup()
 
 mason_lspconfig.setup({
-  ensure_installed = {
-  },
+	ensure_installed = {},
 })
 
 mason_null_ls.setup({
-  ensure_installed = {
-    "prettier",
-    "stylua",
-    "eslint_d"
-  }
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"eslint_d",
+		"omnisharp",
+		"csharpier",
+		"cssls",
+		"docker_compose_language_service",
+		"dockerls",
+		"html",
+		"jsonls",
+		"pylsp",
+		"tsserver",
+	},
 })
