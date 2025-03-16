d08b613873a9c8454f412cf384eb1946a38598a4
Lucas Dell'Isola <lucas.dellisola@rystadenergy.com>
2025-03-16 11:17:23 +0100

    bunch of stuff

--- nvim/lua/lucasd/plugins/copilot-chat.lua
+++ nvim/lua/lucasd/plugins/copilot-chat.lua
@@ -1,18 +1,18 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	branch = "canary",
-	dependencies = {
-		{ "zbirenbaum/copilot.lua" },
-		{ "nvim-lua/plenary.nvim" },
-	},
-	opts = {
-		context = "buffers",
-		window = {
-			layout = "float",
-			relative = "cursor",
-			width = 1,
-			height = 0.4,
-			row = 1,
-		},
-	},
+	-- "CopilotC-Nvim/CopilotChat.nvim",
+	-- branch = "canary",
+	-- dependencies = {
+	-- 	{ "zbirenbaum/copilot.lua" },
+	-- 	{ "nvim-lua/plenary.nvim" },
+	-- },
+	-- opts = {
+	-- 	context = "buffers",
+	-- 	window = {
+	-- 		layout = "float",
+	-- 		relative = "cursor",
+	-- 		width = 1,
+	-- 		height = 0.4,
+	-- 		row = 1,
+	-- 	},
+	-- },
 }
